a96a6c5e538c78cf2fdfb7356be291c8a9e26fc7
mhilmi999 <hilmi.muhammad.rmdhn@gmail.com>
2022-05-17 20:57:46 +0700

    templates include

--- module/routes/routes.go
+++ module/routes/routes.go
@@ -35,7 +35,7 @@ func Init(db *gorm.DB) *gin.Engine {
 	cookieStore := cookie.NewStore([]byte(auth.SECRET_KEY))
 	router.Use(sessions.Sessions("tamaskapju", cookieStore))
 
-	router.HTMLRender = ManualRender("./public/")
+	router.HTMLRender = ManualRender("./public/template/")
 
 	// Routing Website Service
 	// User
