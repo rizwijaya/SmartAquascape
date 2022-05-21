e255f51f735382f4e3c8c98aa4f796edd63ce559
Herwindams24 <herwinda24.ms@gmail.com>
2022-05-21 09:21:48 +0700

    change database

--- smart_aquascape.sql
+++ smart_aquascape.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2022 at 02:00 PM
+-- Generation Time: May 21, 2022 at 04:20 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -106,7 +106,7 @@ CREATE TABLE `sensor_time` (
 --
 
 CREATE TABLE `temperature` (
-  `id` int(11) NOT NULL,
+  `temperature_id` varchar(255) NOT NULL,
   `temperature` float NOT NULL,
   `time` datetime DEFAULT NULL,
   `data` varchar(255) NOT NULL
@@ -119,7 +119,7 @@ CREATE TABLE `temperature` (
 --
 
 CREATE TABLE `turbidity` (
-  `id` int(11) NOT NULL,
+  `turbidity_id` varchar(255) NOT NULL,
   `turbidity` float NOT NULL,
   `time` datetime NOT NULL,
   `data` varchar(255) NOT NULL
@@ -143,6 +143,13 @@ CREATE TABLE `user` (
   `date_created` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_kawasan`, `nama`, `email`, `password`, `avatar`, `id_role`, `date_updated`, `date_created`) VALUES
+(1, 1, 'herwinda', 'herwinda@email.com', 'herwinda24', '', 1, '2022-05-20 13:22:53', '2022-05-20 13:22:53');
+
 -- --------------------------------------------------------
 
 --
@@ -150,7 +157,7 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `water_level` (
-  `id` int(11) NOT NULL,
+  `waterlevel_id` varchar(255) NOT NULL,
   `water_level` float NOT NULL,
   `time` datetime NOT NULL,
   `data` varchar(255) NOT NULL
@@ -182,13 +189,13 @@ ALTER TABLE `sensor_time`
 -- Indexes for table `temperature`
 --
 ALTER TABLE `temperature`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`temperature_id`);
 
 --
 -- Indexes for table `turbidity`
 --
 ALTER TABLE `turbidity`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`turbidity_id`);
 
 --
 -- Indexes for table `user`
@@ -200,7 +207,7 @@ ALTER TABLE `user`
 -- Indexes for table `water_level`
 --
 ALTER TABLE `water_level`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`waterlevel_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -225,28 +232,10 @@ ALTER TABLE `sensor_time`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `temperature`
---
-ALTER TABLE `temperature`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `turbidity`
---
-ALTER TABLE `turbidity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `water_level`
---
-ALTER TABLE `water_level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
