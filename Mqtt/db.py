b08970ac52cb3788504117ac6ec4cd1f3667fe93
Rizqi Wijaya <rizwijaya@github.com>
2022-05-22 16:27:13 +0700

    Button Response and Mapping Data Monitoring MQTT

--- Mqtt/db.py
+++ Mqtt/db.py
@@ -1,19 +1,61 @@
 # python3.6
-from uplink import *
-from downlink import *
+import mysql.connector
+import requests
+
+# Konfigurasi Database
+def connector():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="smartaquascape"
+    )
+    return mydb
+
+def insertMonitoring(dataSensor):
+    head, waterLevel, temperature, turbidity = dataSensor.split(':')
+    turbidity, _ = turbidity.split(';')
+
+    mydb = connector()
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO monitoring (temperature, water_level, turbidity) VALUES (%s, %s, %s)"
+    val = (temperature, waterLevel, turbidity)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return
+
+# def responMonitoring():
+#     mydb = connector()
+#     mycursor = mydb.cursor()
+#     mycursor.execute("SELECT time_deliver FROM deliver_time")
+#     myresult = mycursor.fetchall()
+#     #print(myresult[0][0])
+#     data = '\r\n{\r\n  "m2m:cin": {\r\n    "con": \r\n      \"{\r\n      \t \\"header\\": '+str(4)+',\r\n         \\"waktuPengiriman\\": \\"'+str(myresult[0][0])+'\\"\r\n      }\"\r\n    }\r\n}'
+#     #print(data)
+#     print("\nSending data to Antares...\n")
+#     url = 'https://platform.antares.id:8443/~/antares-cse/antares-id/smartAquascape/TimeMonitoring'
+#     headers = {'cache-control':'no-cache','Content-Type':'application/json;ty=4','X-M2M-Origin':'9fb52249c593c66d:b719370dce7d93b9','Accept':'application/json'}
+#     response = requests.post(url,headers=headers,data=data)
+#     return response
+
+def buttonFeeder(stsFeeder):
+    mydb = connector()
+    mycursor = mydb.cursor()
+    sql = "UPDATE feeder SET statusFeeder = %s WHERE id = %s"
+    val = (stsFeeder, "1")
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return
 
 def dataChecker(data, id):
-    hd = data["data"][0:2]
-    if hd == "11": #Jika sensor data
-        insertHistory(data, id)
-        uplinkDevice(data, id)
-    elif hd == "22" or hd == "23" or hd == "24" or hd == "25": #jika alarm data
-        uplinkAlarm(data, id)
-    elif hd == "33": #jika config data
-        uplinkConfig(data, id)
-    elif hd == "AA": #jika set config data
-        downlinkConfig(data, id)
-    elif hd == "00" or hd == "01" or hd == "BB" or hd == "AB" or hd == "AC" or hd == "AD" or hd == "AF" or hd == "BA": #jika control
-        downlinkRelay(data, id)
-    elif hd == "AE": #jika set listrik
-        downlinkCalibration(data, id)
+    print(data)
+    if data["header"] == 1: #1:WaterLevel:Temperature:Turbidity
+        insertMonitoring(data["data"])
+    elif data["header"] == 9:
+        buttonFeeder(data["statusFeeder"])
+        #print(str(head) + "\n" + str(waterLevel) + "\n" + str(temperature) + "\n" + str(turbidity))
+    # elif data["header"] == 4:
+    #     data = ""
+    # elif data["header"] == 5:
+    #     responMonitoring()
+    
--- perangkat/readme.md
+++ perangkat/readme.md
@@ -16,32 +16,65 @@
 ## Relay
 ```json
 Data Structure:
-Monitoring Sensor
+//Data Monitoring from Device - antares
+Monitoring Sensor //Device Name Topic
 {
     header : 1,
     data    :"1:WaterLevel:Temperature:Turbidity",
     waktuReq : ""
 }
-Konfigure Sending Sensor Data:
-{
+//Get Time Monitoring from device to antares
+// Get Data Sensor Monitoring:
+// {
+//     header : 5,
+//     data   :"GetTimeMonitoring"
+// }
+
+Respon Data Sensor Monitoring: //from website (mqtt service) to antares
+{ //Time Monitoring Topic
     header : 4,
     waktuPengiriman : "", // Waktu pengiriman data Monitoring dalam menit
 }
-Controlling Sensor Feeder:
-{
-    header : 2,
-    status : "",
-    statusFeeder: "",
-    waktuReq : ""
+
+//Cek Status Feeder
+// Check Status Feeder: //from device to antares
+// {
+//     header : 6,
+//     data : "FeederStatus"
+// }
+Respon Status Feeder //from website (mqtt service) to antares
+{ ///sts feeeder topic
+    header : 7,
+    statusControl : 1 //sts 1 untk time, sts 2 untk manual.
 }
-Trigger Sensor By Time:
-{
-    header : 3,
-    status: "",
-    waktuON_jam : "",
-    waktuON_menit : "",
-    waktuOFF_jam : "",
-    waktuOFF_menit : "",
-    waktuReq : ""
+
+//Controlling Feeder Manual
+// Check Status Feeder Manual { //From device to antares
+//     header : 8,
+//     data : "StatusFeederManual"
+// }
+
+Respon Status Feeder Manual { //From website service to antares //ManFeeder Topic
+    header : 9,
+    statusFeeder : 1 // jika 1 feeder aktif/button ditekan, jika 0 feeder tidak aktif/button tidak ditekan
+}
+
+Request Disable Feeder From Perangkat to DB { //From device to antares //ManFeeder Topic
+    header : 9,
+    statusFeeder : 0
+}
+//Request Data Automatic Feeder Nyala dan Mati dengan 2 data:
+// Request Data Automatic Feeder //From device to antares
+// {
+//     header : 11,
+//     data : "AutomaticFeeder"
+// }
+
+Respon Data Feeder Automatic //From website (mqtt service) to antares
+{ //auFeeder Topic
+    header : 12,
+    waktuPertama : "HHMM",
+    waktuKedua : "HHMM",
+    waktuKetiga : "HHMM"
 }
 ```
